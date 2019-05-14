334a22a230e778b8c94a138d122e4adeb27456ad
dmmiller612 <dmmiller612@gmail.com>
2019-05-14 08:56:42 -0400

    Update spacy

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='bert-extractive-summarizer',
-      version='0.1.2',
+      version='0.1.3',
       description='Extractive Text Summarization with BERT',
       keywords = ['bert', 'pytorch', 'machine learning', 'deep learning', 'extractive summarization', 'summary'],
       long_description=open("README.md", "r", encoding='utf-8').read(),
       long_description_content_type="text/markdown",
       url='https://github.com/dmmiller612/bert-extractive-summarizer',
-      download_url='https://github.com/dmmiller612/bert-extractive-summarizer/archive/0.1.2.tar.gz',
+      download_url='https://github.com/dmmiller612/bert-extractive-summarizer/archive/0.1.3.tar.gz',
       author='Derek Miller',
       author_email='dmmiller612@gmail.com',
-      install_requires=['pytorch-pretrained-bert', 'sklearn', 'neuralcoref', 'spacy'],
+      install_requires=['pytorch-pretrained-bert', 'sklearn', 'neuralcoref', 'spacy <= 2.3.1'],
       license='MIT',
       packages=find_packages(),
       zip_safe=False)

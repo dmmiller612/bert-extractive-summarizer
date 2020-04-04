6f9617b397d3a748969da79dc4c0e08c75c1717a
dmmiller612 <dmmiller612@gmail.com>
2020-04-04 15:15:33 -0400

    Update setup

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='bert-extractive-summarizer',
-      version='0.4.0',
+      version='0.4.1',
       description='Extractive Text Summarization with BERT',
       keywords = ['bert', 'pytorch', 'machine learning', 'deep learning', 'extractive summarization', 'summary'],
       long_description=open("README.md", "r", encoding='utf-8').read(),
       long_description_content_type="text/markdown",
       url='https://github.com/dmmiller612/bert-extractive-summarizer',
-      download_url='https://github.com/dmmiller612/bert-extractive-summarizer/archive/0.4.0.tar.gz',
+      download_url='https://github.com/dmmiller612/bert-extractive-summarizer/archive/0.4.1.tar.gz',
       author='Derek Miller',
       author_email='dmmiller612@gmail.com',
       install_requires=['transformers', 'scikit-learn', 'spacy'],

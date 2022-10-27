f9e4ff7a39b1a6e1824df9049583346f2213af97
Phil Gooch <phil@scholarcy.com>
2022-10-27 09:23:12 +0100

    * Improves coreferent-antecedent replacement based on discussions in https://github.com/explosion/spaCy/discussions/11585#discussioncomment-3976259

--- summarizer/text_processors/coreference_handler.py
+++ summarizer/text_processors/coreference_handler.py
@@ -18,7 +18,7 @@ class CoreferenceHandler(SentenceABC):
         self, spacy_model: str = DEFAULT_MODEL
     ):
         """
-        Coreference handler. Updated to work with spacy > 3.0.
+        Coreferennce handler. Updated to work with spacy > 3.0.
 
         :param spacy_model: The spacy model to use as default.
         """
@@ -37,11 +37,20 @@ class CoreferenceHandler(SentenceABC):
         doc = self.nlp(body)
         resolved_text = body
         offset = 0
+        reindex = []
         for chain in doc.spans:
             for idx, span in enumerate(doc.spans[chain]):
                 if idx > 0:
-                    resolved_text = resolved_text[0:span.start_char + offset] + doc.spans[chain][0].text + resolved_text[span.end_char + offset:]
-                    offset += len(doc.spans[chain][0].text) - (span.end_char - span.start_char)
+                    reindex.append([span.start_char, span.end_char, doc.spans[chain][0].text, span.text])
+
+        for span in sorted(reindex, key=lambda x: x[0]):
+            antecedent = span[2]
+            coreferent = span[3]
+            if coreferent in {'its', 'his', 'her', 'their', 'His', 'Her', 'Their', 'Its'}:
+                antecedent = antecedent + "\u2019s"
+            resolved_text = resolved_text[0:span[0] + offset] + antecedent + resolved_text[span[1] + offset:]
+            offset += len(antecedent) - (span[1] - span[0])
+
         result_sents = SentenceHandler().process(resolved_text, min_length=min_length)
         return result_sents
 
